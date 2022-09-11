bc224309cc80bd99e955341f2baa2b245bed1f9b
oalmalky5 <oalmalky5@gmail.com>
2022-09-11 21:36:41 +0300

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+// in this file we are connecting the index file in the client side with one in the server side
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('root'))
